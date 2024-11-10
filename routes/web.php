6184f9ce80b45eb21b7eee471340a8c7eab9cc9f
andressa pinheiros <ferreirapinheiros04@gmail.com>
2024-11-10 14:43:24 -0300

    tela de login

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LoginController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\NotaController;
 use App\Http\Controllers\CategoriaController;
@@ -7,9 +9,15 @@ use App\Http\Controllers\TagController;
 use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+
+Route::get('/', [HomeController::class, 'index'])->name('home');
+
+Route::controller(LoginController::class)->group(function () {
+  Route::get('/login', 'index')->name('login.index');
+  Route::post('/login', 'store')->name('login.store');
+  Route::get('/logout', 'destroy')->name('login.destroy');
 });
+
 
 Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
